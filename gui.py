5a996c4b3e065d04e8ed75bde73197a56a5751a3
Alex Hill <alexhill1998@gmail.com>
2024-06-10 19:04:47 +0300

    gui first steps

--- gui.py
+++ gui.py
@@ -4,8 +4,9 @@ import FreeSimpleGUI as sg
 lable =sg.Text("Type in a To-Do")
 input_box =sg.InputText(tooltip="Enter To-Do")
 add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 
-
-window = sg.Window("My To-Do App",layout=[[lable], [input_box , add_button]])
+window = sg.Window("My To-Do App",layout=[[lable], [input_box, add_button], [edit_button, complete_button]])
 window.read()
 window.close()
